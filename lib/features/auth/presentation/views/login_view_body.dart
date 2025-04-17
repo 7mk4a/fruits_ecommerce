e0ff6d9836a3a95c1bd45d7d4bf1a1842a6d9f5c
AhmedWael <ahmed1221.wael@gmail.com>
2025-04-17 10:49:07 +0200

    added sign in button and forget password and create account

--- lib/features/auth/presentation/views/login_view_body.dart
+++ lib/features/auth/presentation/views/login_view_body.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:fruits_ecommerce/constants.dart';
+import 'package:fruits_ecommerce/core/utils/app_colors.dart';
+import 'package:fruits_ecommerce/core/utils/app_text_styles.dart';
+import 'package:fruits_ecommerce/core/widgets/custom_button.dart';
 import 'package:fruits_ecommerce/core/widgets/custom_text_form_field.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -22,6 +26,46 @@ class LoginViewBody extends StatelessWidget {
               hintText: "كلمة المرور",
               textInputType: TextInputType.emailAddress,
               suffixIcon: Icon(Icons.remove_red_eye, color: Color(0XFFC9CECF)),
+            ),
+            SizedBox(height: 33),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'هل نسيت كلمة المرور',
+                  style: TextStyles.semiBold13.copyWith(
+                    color: AppColors.lightPrimaryColor,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 33),
+            CustomButton(onPressed: () {}, text: 'تسجيل الدخول'),
+            SizedBox(height: 33),
+            Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'لا تمتلك حساب؟',
+                    style: TextStyles.semiBold16.copyWith(
+                      color: const Color(0xFF949D9E),
+                    ),
+                  ),
+                  TextSpan(
+                    text: ' ',
+                    style: TextStyles.semiBold16.copyWith(
+                      color: const Color(0xFF616A6B),
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'قم بإنشاء حساب',
+                    style: TextStyles.semiBold16.copyWith(
+                      color: AppColors.primaryColor,
+                    ),
+                  ),
+                ],
+              ),
+              textAlign: TextAlign.center,
             ),
           ],
         ),
