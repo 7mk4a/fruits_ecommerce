08043a928f2a944fd5695a5a9f11bdb8f6274e01
AhmedWael <ahmed1221.wael@gmail.com>
2025-04-18 08:46:45 +0200

    added firebase and removed the api from git and finished signup UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_ecommerce/core/helper_functions/on_generate_routes.dart';
 import 'package:fruits_ecommerce/core/services/shared_preferences_singleton.dart';
 import 'package:fruits_ecommerce/core/utils/app_colors.dart';
 import 'package:fruits_ecommerce/features/splash/presentation/views/splash_view.dart';
+import 'package:fruits_ecommerce/firebase_options.dart';
 import 'package:fruits_ecommerce/generated/l10n.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await Prefs.init();
   runApp(const FriutsApp());
 }
