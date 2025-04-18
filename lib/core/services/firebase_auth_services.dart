9e88d483a885b01e55512ba7373b0cb44cd86eff
AhmedWael <ahmed1221.wael@gmail.com>
2025-04-18 09:30:02 +0200

    created custom exception and added the firebaseAuthServices and added the custom exceptions to it

--- lib/core/services/firebase_auth_services.dart
+++ lib/core/services/firebase_auth_services.dart
@@ -1,1 +1,27 @@
-class FirebaseAuthServices {}
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fruits_ecommerce/core/errors/exceptions.dart';
+
+class FirebaseAuthServices {
+  Future<User> createUserWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      final credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw CustomException(message: 'The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        throw CustomException(
+          message: 'The account already exists for that email.',
+        );
+      } else {
+        throw CustomException(message: 'An error occurred Please Try Again');
+      }
+    } catch (e) {
+      throw CustomException(message: 'An error occurred Please Try Again');
+    }
+  }
+}
